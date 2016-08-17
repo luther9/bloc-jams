cd2b8826983375a59f04a167cc817805bdb8f88f
Luther <lutheroto@gmail.com>
2016-08-17 07:01:47 -0400

    Add JavaScript.

---
+++ scripts/landing.js
@@ -0,0 +1,14 @@
+// Original code is in Checkpoint 22.
+// Why is this a var instead of a function declaration?
+var animatePoints = function() {
+
+  var points = document.getElementsByClassName('point')
+
+  var transform = 'scaleX(1) translateY(0)'
+  for (var i = 0; i < 3; ++i) {
+    points[i].style.opacity = 1
+    points[i].style.transform = transform
+    points[i].style.msTransform = transform
+    points[i].style.WebkitTransform = transform
+  }
+}
